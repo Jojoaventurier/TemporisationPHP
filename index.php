da5a2677ec65a90836855bb91d7c7369763a9e81
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-05-31 10:50:50 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    ob_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajout produit</title>
+</head>
+<body>
+    <div id=wrapper>
+        <h1>Ajouter produit</h1>
+        
+    </div>    
+
+</body>
+</html>
